2ec302cb34843e2c174e070f41ab4bab32a0aa66
reshmavm96 <86783775+reshmavm96@users.noreply.github.com>
2021-07-02 17:12:57 +0530

    Add files via upload

---
+++ invoice/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from django.forms import formset_factory
+from .models import Invoice
+class InvoiceForm(forms.Form):
+    
+    
+    customer = forms.CharField(
+        label='Cusomter',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Customer/Company Name',
+            'rows':1
+        })
+    )
+    customer_email = forms.CharField(
+        label='Customer Email',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'customer@company.com',
+            'rows':1
+        })
+    )
+    billing_address = forms.CharField(
+        label='Billing Address',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'rows':1
+        })
+    )
+    message = forms.CharField(
+        label='Message/Note',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'message',
+            'rows':1
+        })
+    )
+
+class LineItemForm(forms.Form):
+    
+    service = forms.CharField(
+        label='Service/Product',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control input',
+            'placeholder': 'Service Name'
+        })
+    )
+    description = forms.CharField(
+        label='Description',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control input',
+            'placeholder': 'Enter Book Name here',
+            "rows":1
+        })
+    )
+    quantity = forms.IntegerField(
+        label='Qty',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control input quantity',
+            'placeholder': 'Quantity'
+        }) 
+    )
+    rate = forms.DecimalField(
+        label='Rate $',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control input rate',
+            'placeholder': 'Rate'
+        })
+    )
+   
+    
+LineItemFormset = formset_factory(LineItemForm, extra=1)
